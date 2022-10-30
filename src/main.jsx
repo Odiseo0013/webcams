b605f79b2e99fc27ed9a86e87aa8925f0eff91e3
Odiseo0013 <63138182+Odiseo0013@users.noreply.github.com>
2022-10-30 22:29:28 +0100

    subida

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Webcams } from './assets/components/Webcams'
+import Webcams  from './assets/components/Webcams'
 import './assets/css/styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
